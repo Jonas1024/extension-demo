185b3590956f6c7bec3370648e5baaad7a35c055
jianrong <jianrong.fan@maplequad.com>
2024-11-12 14:11:59 +0800

    init

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,36 +1,52 @@
-
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 let currentWindow = null;
-chrome.windows.onRemoved.addListener(
-  (windowId) => {
-    if (currentWindow?.id === windowId) {
-      currentWindow = null;
-    }
+chrome.windows.onRemoved.addListener((windowId) => {
+  if (currentWindow?.id === windowId) {
+    currentWindow = null;
   }
-)
-chrome.runtime.onMessage.addListener(async request => {
+});
+chrome.runtime.onMessage.addListener(async (request) => {
   if (request.type === "OpenAuth") {
     if (currentWindow) {
       await chrome.windows.remove(currentWindow.id);
     }
-    const data = request.href.includes('?i_m=')
-      ? { type: 'base64', payload: request.href.split('?i_m=')[1] }
-      : { type: 'link', payload: decodeURIComponent(request.href.split('?request_uri=')[1]) };
+    chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
+      const data = request.href.includes("?i_m=")
+        ? { type: "base64", payload: request.href.split("?i_m=")[1] }
+        : {
+            type: "link",
+            payload: decodeURIComponent(request.href.split("?request_uri=")[1]),
+          };
 
-    chrome.windows.create({
-      url: chrome.runtime.getURL(`index.html#/auth?type=${data.type}&payload=${data.payload}`),
-      type: "popup",
-      focused: true,
-      width: 390,
-      height: 600,
-      top: 0,
-      left: request.windowWidth - 390,
-    }, () => {
-      console.log("Opened popup!")
-    })
-    
+      // 窗口大小设置
+      const windowWidth = 390;
+      const windowHeight = 600;
+
+      // 计算右上角的位置
+      const left = currentWindow.left + currentWindow.width - windowWidth;
+      const top = currentWindow.top;
+
+      chrome.windows.create(
+        {
+          url: chrome.runtime.getURL(
+            `index.html#/auth?type=${data.type}&payload=${data.payload}`
+          ),
+          type: "popup",
+          focused: true,
+          width: windowWidth,
+          height: windowHeight,
+          left: left,
+          top: top,
+        },
+        () => {
+          console.log("Opened popup!");
+        }
+      );
+    });
+    // 返回 true 表示异步响应
+    return true;
   }
 });
--- src/constants/common.constants.js
+++ src/constants/common.constants.js
@@ -1,5 +1,5 @@
 export const RHS_URL = 'https://rhs-staging.polygonid.me';
-export const RPC_URL = '';
+export const RPC_URL = 'https://rpc-amoy.polygon.technology/';
 
 export const defaultEthConnectionConfig =  [{
 	url: RPC_URL,
